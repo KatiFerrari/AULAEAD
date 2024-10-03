82c60d3bd5db7003ba93742872b199a205a9c222
mariafer057 <mariafernandaalves057@gmail.com>
2024-10-03 14:28:43 -0400

    dia-03-10

---
+++ src/app/Perfil/page.tsx
@@ -0,0 +1,15 @@
+import {useState} from "react";
+import Usuario from "../interfaces/usuario"
+import PerfilUsuario from "../interfaces/usuario"
+
+const PaginaPerfil = () =>{
+    const [usuario, setUsuario] = useState <Usuario>() setUsuario((nome:'Billie Eilish', idade:20, email:))
+   
+    return(
+        <div>
+            <h1>Página Perfil</h1>
+        </div>
+    )
+}
+
+export default  PaginaPerfil;
